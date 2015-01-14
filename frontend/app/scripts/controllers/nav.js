87a7b321708b724d0b7f27a481a6366399a65516
Richard Lewis <rl403@cam.ac.uk>
2015-01-14 02:47:37 +0000

    scaffolding the app

---
+++ frontend/app/scripts/controllers/nav.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:NavcontrollerCtrl
+ * @description
+ * # NavcontrollerCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('NavCtrl', function ($scope) {
+    $scope.awesomeThings = [
+    	
+    ];
+  });
